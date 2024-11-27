8032b38edb719710cdc24b8853c94bf2cde83f28
leonardo-luz <leonardoluz.f02@gmail.com>
2024-11-27 16:27:59 -0300

    [feat] added basic router configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // errorElement: <Error />,
+    children: [
+      // {
+      //   index: true,
+      //   element: <Home />
+      // }
+    ]
+  }
+])
 const root = createRoot(document.getElementById('root')!)
 
-root.render(
-  <App />
-)
+root.render(<RouterProvider router={router} />)
