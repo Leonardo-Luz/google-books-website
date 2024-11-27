a8c5c29e346c27afb1eb3d75c36eed7b61a4d5cb
leonardo-luz <leonardoluz.f02@gmail.com>
2024-11-27 16:52:03 -0300

    [feat] added nav and footer to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 
+import { Nav } from './components/Nav.component'
+import { Footer } from './components/Footer.component'
+
 function App() {
   return (
     <>
+      <Nav />
 
       <Outlet />
 
+      <Footer />
     </>
   )
 }
