6d33d586031e5268b4f36b22cebaf3bcda5f3994
leonardo-luz <leonardoluz.f02@gmail.com>
2024-11-27 16:36:48 -0300

    [feat] route index config

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Home } from './routes/Home.route.tsx'
+import { Home, Error } from './routes'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    // errorElement: <Error />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
