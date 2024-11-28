67e39e42b5793fb2b8692fcffeec2214a20cfcf5
leonardo-luz <leonardoluz.f02@gmail.com>
2024-11-28 16:19:23 -0300

    [feat] added basic style

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import { Outlet } from 'react-router-dom'
-import './App.css'
 
 import { Nav } from './components/Nav.component'
 import { Footer } from './components/Footer.component'
 
 function App() {
   return (
-    <>
+    <div className='main-container'>
       <Nav />
 
       <Outlet />
 
       <Footer />
-    </>
+    </div>
   )
 }
 
