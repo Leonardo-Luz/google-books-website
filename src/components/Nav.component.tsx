20e29b9a116f94bbe77d95599fb4faff8c5b3fa1
leonardo-luz <leonardoluz.f02@gmail.com>
2024-11-28 16:34:58 -0300

    [feat] added basic nav style

--- src/components/Nav.component.tsx
+++ src/components/Nav.component.tsx
@@ -1,7 +1,9 @@
+import '../styles/nav.style.css'
+
 export const Nav = () => {
   return (
-    <div>
-      <p>NAV</p>
+    <div className='nav-container'>
+      <h1 className='nav-title'>NAV</h1>
     </div>
   )
 }
