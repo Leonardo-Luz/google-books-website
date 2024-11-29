657e8278fc8e44072290236413e32b6ac00bcbbb
leonardo-luz <leonardoluz.f02@gmail.com>
2024-11-28 23:51:55 -0300

    [feat] added all book data

--- src/routes/Book.route.tsx
+++ src/routes/Book.route.tsx
@@ -6,6 +6,20 @@ import DOMPurify from "dompurify";
 
 import '../styles/book.style.css'
 
+type bookElementProps = {
+  title: string,
+  data: string
+}
+const BookElement = ({ title, data }: bookElementProps) =>
+  data != 'undefined' ?
+    <label className="book-element">
+      <p>{title}:</p>
+      <p className="dashes"></p>
+      <p className="book-data-info">{data}</p>
+    </label>
+    :
+    <></>
+
 export const Book = () => {
   const { search } = useParams();
 
@@ -30,7 +44,19 @@ export const Book = () => {
           <div className="book-container" key={book.id}>
             <p>{book.volumeInfo.title}</p>
             <img src={book.volumeInfo.imageLinks.thumbnail} />
-            <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(book.volumeInfo.description) }} />
+            <div
+              dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(book.volumeInfo.description) }}
+              className="book-description"
+            />
+            <div className="book-data">
+              <BookElement title="Publisher" data={book.volumeInfo.publisher} />
+              <BookElement title="Published Date" data={book.volumeInfo.publishedDate} />
+              <BookElement title="Maturity Rating" data={book.volumeInfo.maturityRating} />
+              <BookElement title="Categories" data={book.volumeInfo.categories.join(' / ')} />
+              <BookElement title="Rating" data={String(book.volumeInfo.averageRating)} />
+              <BookElement title="Rating Count" data={String(book.volumeInfo.ratingsCount)} />
+              <BookElement title="Price" data={String(book.saleInfo.listPrice ? `${book.saleInfo.listPrice.amount} ${book.saleInfo.listPrice.currencyCode}` : undefined)} />
+            </div>
           </div>
           :
           <p>LOADING</p>
